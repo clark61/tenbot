f9aed69f3a1bb366e0897e067ced7a6034fc91cb
clark61 <Clark6147@gmail.com>
2022-07-10 03:52:31 -0500

    Refactor and add command to get most recent results

--- src/commands/f1.rs
+++ src/commands/f1.rs
@@ -1,6 +1,6 @@
 use crate::commands::util;
 use serde_json::Value;
-use serenity::builder::{CreateEmbed, CreateEmbedFooter};
+use serenity::builder::CreateEmbed;
 use serenity::client::Context;
 use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
 use serenity::utils::{Colour, MessageBuilder};
@@ -93,7 +93,7 @@ async fn get_season_calendar() -> SeasonCalendar {
     let v: Value = serde_json::from_str(&data).unwrap();
     let info = &v["MRData"]["RaceTable"]["Races"];
     let total_rounds = get_total_rounds().await;
-    let current_season = get_season_year(&info).to_string();
+    let current_season = get_season_year(&info);
 
     for i in 0..total_rounds as usize {
         let rounds = info[i]["round"].to_string().replace("\"", "");
@@ -117,6 +117,40 @@ async fn get_season_calendar() -> SeasonCalendar {
     season_calendar
 }
 
+/// Collects json response from Ergast API call to get the results from the most recent GP
+async fn get_recent_race_results() -> Standings {
+    let url = "https://ergast.com/api/f1/current/last/results.json";
+    let mut driver_names = MessageBuilder::new();
+    let mut driver_constructors = MessageBuilder::new();
+    let mut driver_points = MessageBuilder::new();
+    let standings: Standings;
+
+    let data = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let v: Value = serde_json::from_str(&data).unwrap();
+    let info = &v["MRData"]["RaceTable"]["Races"][0]["Results"];
+    let total_drivers: u8 = v["MRData"]["total"].as_str().unwrap().parse().unwrap();
+
+    for i in 0..total_drivers as usize {
+        let points = info[i]["points"].to_string().replace("\"", "");
+        let constructor = info[i]["Constructor"]["name"].to_string().replace("\"", "");
+        let driver = info[i]["Driver"]["familyName"]
+            .to_string()
+            .replace("\"", "");
+
+        driver_names.push(format!("{}\n", driver));
+        driver_constructors.push(format!("{}\n", constructor));
+        driver_points.push(format!("{}\n", points));
+    }
+
+    standings = Standings {
+        drivers: Some(driver_names),
+        constructors: Some(driver_constructors),
+        points: Some(driver_points),
+    };
+
+    standings
+}
+
 /// Return the total amount of races for the current season
 async fn get_total_rounds() -> u8 {
     let url = "https://ergast.com/api/f1/current.json";
@@ -127,9 +161,23 @@ async fn get_total_rounds() -> u8 {
     total
 }
 
+/// Returns the name of the most recent GP
+async fn get_recent_race_name() -> String {
+    let url = "https://ergast.com/api/f1/current/last/results.json";
+
+    let data = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let v: Value = serde_json::from_str(&data).unwrap();
+
+    let race_name = v["MRData"]["RaceTable"]["Races"][0]["raceName"]
+        .as_str()
+        .unwrap()
+        .to_string();
+    race_name
+}
+
 /// Returns the most recent season's year
-fn get_season_year(info: &Value) -> &str {
-    info[0]["season"].as_str().unwrap()
+fn get_season_year(info: &Value) -> String {
+    info[0]["season"].as_str().unwrap().to_string()
 }
 
 /// Retrieves F1 constructor standings and outputs results through an embedded message
@@ -177,11 +225,6 @@ pub async fn season_calendar(ctx: Context, command: ApplicationCommandInteractio
     // Collect season info
     let calendar = get_season_calendar().await;
 
-    // Create footer
-    let mut footer = CreateEmbedFooter::default();
-    footer.text("https://f1calendar.com/");
-    footer.icon_url("https://raw.githubusercontent.com/sportstimes/f1/9cdaa32dba300930b944bc739517063147cae5b2/_public/f1/mstile-70x70.png");
-
     // Format embedded message
     let mut embed = CreateEmbed::default();
     embed.title(format!("{} Season Calendar", calendar.season_year));
@@ -198,4 +241,23 @@ pub async fn season_calendar(ctx: Context, command: ApplicationCommandInteractio
     util::generate_embed_message(ctx, command, embed).await
 }
 
-// https://ergast.com/api/f1/current/last/results.json
+pub async fn recent_race_results(ctx: Context, command: ApplicationCommandInteraction) {
+    // Collect the last race's results
+    let standings = get_recent_race_results().await;
+    let race_name = get_recent_race_name().await;
+
+    // Format embedded message
+    let mut embed = CreateEmbed::default();
+    embed.title(format!("{} Results", race_name));
+    embed.colour(Colour::DARK_RED);
+    embed.thumbnail("https://1000logos.net/wp-content/uploads/2020/02/F1-Logo-500x281.png");
+    embed.field("Name", standings.drivers.unwrap(), true);
+    embed.field("Constructor", standings.constructors.unwrap(), true);
+    embed.field("Points", standings.points.unwrap(), true);
+    embed.footer(|footer| {
+        footer.text("Message formatting may appear inconsistent on smaller screens")
+    });
+
+    // Attempt to send response
+    util::generate_embed_message(ctx, command, embed).await
+}
